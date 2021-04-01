a81f8da5480ad007f5846ccbd565dbb607af3729
admin <917719801@qq.com>
2021-04-01 17:04:26 +0800

    练习1

---
+++ src/test/java/com/testcase/JunitDemo01Test.java
@@ -0,0 +1,14 @@
+package com.testcase;
+
+import org.junit.jupiter.api.Test;
+
+public class JunitDemo01Test {
+    @Test
+    void fun1(){
+        System.out.println("fun1");
+    }
+    @Test
+    void fun2(){
+        System.out.println("fun2");
+    }
+}
