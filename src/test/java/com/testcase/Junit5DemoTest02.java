399c506e9172a27cea9eccc09044d5fc85e213b2
admin <917719801@qq.com>
2021-04-07 19:44:04 +0800

    alluer使用

---
+++ src/test/java/com/testcase/Junit5DemoTest02.java
@@ -0,0 +1,19 @@
+package com.testcase;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Junit5DemoTest02 {
+    @Test
+    void assertion(){
+        assertAll("demo",
+                ()-> assertEquals(1,2),
+                ()-> assertEquals(1,1),
+                ()-> assertEquals(1,4),
+                ()-> assertEquals(1,5)
+        );
+
+    }
+}
