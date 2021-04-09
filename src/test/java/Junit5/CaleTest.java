1b83a2327aa67677be2a23c87ebd273727458995
admin <917719801@qq.com>
2021-04-09 11:04:24 +0800

    allure报告练习

--- src/test/java/Junit5/CaleTest.java
+++ src/test/java/Junit5/CaleTest.java
@@ -1,5 +1,8 @@
 package Junit5;
 
+import io.qameta.allure.Allure;
+import io.qameta.allure.Link;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,8 +10,17 @@ import static org.junit.jupiter.api.Assertions.*;
 class CaleTest {
 
     @Test
+    @Tag("xxx")
+    @Link("https://www.baidu.com")
+    @Link(name="allure",type = "mylink")
+
     void dic() {
+        Allure.step("第一步");
         Cale cale = new Cale();
+        Allure.step("第2步");
+        Allure.addAttachment("xxxxxx","11111");
+        Allure.addAttachment("demo","image/png",
+                this.getClass().getResourceAsStream("/微信图片_20210318112005.jpg"),"jpg");
         assertEquals(cale.dic(2,1),2);
     }
 }
